3c793ee6d66cd1c4893d1dbc7d68725acb0690e3
NickXiong <xiongmc333@163.com>
2019-02-28 14:01:06 +0800

    AngularJS控制器抽取

---
+++ pinyougou-manager-web/src/main/webapp/js/controller/baseController.js
@@ -0,0 +1,33 @@
+app.controller('baseController',function($scope){
+
+    //刷新列表
+    $scope.reloadList=function(){
+        $scope.search($scope.paginationConf.currentPage,$scope.paginationConf.itemsPerPage);
+    };
+
+    //分页组件相关属性配置
+    $scope.paginationConf={
+        currentPage: 1,
+        totalItems: 10,
+        itemsPerPage: 10,
+        perPageOptions: [10, 20, 30, 40, 50],
+        onChange: function(){
+            $scope.reloadList();//重新加载
+        }
+    };
+
+
+    //定义存放删除元素id的数组
+    $scope.delIds=[];
+
+    //删除元素id数组 维护
+    $scope.updateSelectIds=function($event,id){
+        if($event.target.checked){
+            $scope.delIds.push(id);
+        }else{
+            var index = $scope.delIds.indexOf(id);
+            $scope.delIds.splice(index,1);
+        }
+    };
+
+})
--- pinyougou-manager-web/src/main/webapp/js/controller/brandController.js
+++ pinyougou-manager-web/src/main/webapp/js/controller/brandController.js
@@ -1,4 +1,7 @@
-app.controller('brandController',function($scope,$http,brandService){
+app.controller('brandController',function($scope,$controller,brandService){
+
+    $controller('baseController',{$scope:$scope});//继承
+
     //查询所有
     $scope.findAll=function(){
         brandService.findAll().success(
@@ -7,23 +10,6 @@ app.controller('brandController',function($scope,$http,brandService){
             }
         )
     };
-
-    //分页组件相关属性配置
-    $scope.paginationConf={
-        currentPage: 1,
-        totalItems: 10,
-        itemsPerPage: 10,
-        perPageOptions: [10, 20, 30, 40, 50],
-        onChange: function(){
-            $scope.reloadList();//重新加载
-        }
-    };
-
-    //刷新列表
-    $scope.reloadList=function(){
-        $scope.search($scope.paginationConf.currentPage,$scope.paginationConf.itemsPerPage);
-    };
-
 
     //新增品牌
     $scope.save=function(){
@@ -50,19 +36,6 @@ app.controller('brandController',function($scope,$http,brandService){
                 $scope.entity = response;
             }
         );
-    };
-
-    //定义存放删除元素id的数组
-    $scope.delIds=[];
-
-    //删除元素id数组 维护
-    $scope.updateSelectIds=function($event,id){
-        if($event.target.checked){
-            $scope.delIds.push(id);
-        }else{
-            var index = $scope.delIds.indexOf(id);
-            $scope.delIds.splice(index,1);
-        }
     };
 
     //删除品牌
